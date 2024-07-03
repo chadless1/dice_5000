3c34f33dc4fae409e4efd88f48f5fec7e2a59a7e
chadless1 <chadless1@gmail.com>
2024-07-03 17:27:40 -0400

    updated for mobile screen and added roll score

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
-		
+
+// on window load
+window.onload = function() {
+        //pass
+}
+
 // Function to roll the dice 
 function rollTheDice() { 
-        setTimeout(function () { 
+        setTimeout(function() { 
                 
                 //get random number for dice
-                var randomNumber1 = Math.floor(Math.random() * 6) + 1; 
-                var randomNumber2 = Math.floor(Math.random() * 6) + 1; 
-                var randomNumber3 = Math.floor(Math.random() * 6) + 1; 
-                var randomNumber4 = Math.floor(Math.random() * 6) + 1; 
-                var randomNumber5 = Math.floor(Math.random() * 6) + 1; 
-                var randomNumber6 = Math.floor(Math.random() * 6) + 1; 
+                let randomNumber1 = Math.floor(Math.random() * 6) + 1; 
+                let randomNumber2 = Math.floor(Math.random() * 6) + 1; 
+                let randomNumber3 = Math.floor(Math.random() * 6) + 1; 
+                let randomNumber4 = Math.floor(Math.random() * 6) + 1; 
+                let randomNumber5 = Math.floor(Math.random() * 6) + 1; 
+                
+                let d1Value = randomNumber1;
+                let d2Value = randomNumber2;
+                let d3Value = randomNumber3;
+                let d4Value = randomNumber4;
+                let d5Value = randomNumber5;
                 
                 //dice roll aniation
                 document.querySelector(".img1").setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
@@ -17,7 +27,6 @@ function rollTheDice() {
                 document.querySelector(".img3").setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
                 document.querySelector(".img4").setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
                 document.querySelector(".img5").setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
-                document.querySelector(".img6").setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
 
                 // set random number and display dice
                 document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png"); 
@@ -25,19 +34,22 @@ function rollTheDice() {
                 document.querySelector(".img3").setAttribute("src", "images/dice" + randomNumber3 + ".png"); 
                 document.querySelector(".img4").setAttribute("src", "images/dice" + randomNumber4 + ".png"); 
                 document.querySelector(".img5").setAttribute("src", "images/dice" + randomNumber5 + ".png"); 
-                document.querySelector(".img6").setAttribute("src", "images/dice" + randomNumber6 + ".png"); 
-                
-                setTimeout(function () {
+
+                setTimeout(function() {
                         //clear animation
                         document.querySelector(".img1").setAttribute("style", "none;");
                         document.querySelector(".img2").setAttribute("style", "none;");
                         document.querySelector(".img3").setAttribute("style", "none;");
                         document.querySelector(".img4").setAttribute("style", "none;");
                         document.querySelector(".img5").setAttribute("style", "none;");
-                        document.querySelector(".img6").setAttribute("style", "none;");
 
 
                 }, 1000)
+
+                // set score 
+                let diceSum = (d1Value + d2Value + d3Value + d4Value + d5Value);
+                let rollScore = document.querySelector(".rollScore");
+                rollScore.innerHTML = diceSum; 
 
         }, 1000); 
 
