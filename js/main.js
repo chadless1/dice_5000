bfd4da8f88ea3c3365bfdf13b7dea5aa4d1272ff
chadless1 <chadless1@gmail.com>
2024-07-07 01:15:26 -0400

    updated dice spacing

--- js/main.js
+++ js/main.js
@@ -1,13 +1,51 @@
 
+// set dice
+let d1 = document.querySelector(".img1");
+let d2 = document.querySelector(".img2");
+let d3 = document.querySelector(".img3");
+let d4 = document.querySelector(".img4");
+let d5 = document.querySelector(".img5");
+
+// set dice list
+const diceList = [d1, d2, d3, d4, d5];
+
+// set roll count
+let rollCount = 0;
+
+// set roll score
+let score = 0;
+let rollScore = document.querySelector(".rollScore");
+
 // on window load
 window.onload = function() {
         //pass
+}
+
+// function to create random number list
+
+// calculate score
+
+function calScore() {
+
+  for (let i = 0; i < diceList.length; i++){
+          // pass
+  }              
+
 }
 
 // Function to roll the dice 
 function rollTheDice() { 
         setTimeout(function() { 
                 
+                // add to roll count
+                rollCount += 1
+
+                // check for roll count
+                if (rollCount > 1) {
+                     alert("roll count is greater than 1"); 
+                }
+
+                else {
                 //get random number for dice
                 let randomNumber1 = Math.floor(Math.random() * 6) + 1; 
                 let randomNumber2 = Math.floor(Math.random() * 6) + 1; 
@@ -15,43 +53,36 @@ function rollTheDice() {
                 let randomNumber4 = Math.floor(Math.random() * 6) + 1; 
                 let randomNumber5 = Math.floor(Math.random() * 6) + 1; 
                 
-                let d1Value = randomNumber1;
-                let d2Value = randomNumber2;
-                let d3Value = randomNumber3;
-                let d4Value = randomNumber4;
-                let d5Value = randomNumber5;
+                // dice roll aniation
+                d1.setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
+                d2.setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
+                d3.setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
+                d4.setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
+                d5.setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
                 
-                //dice roll aniation
-                document.querySelector(".img1").setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
-                document.querySelector(".img2").setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
-                document.querySelector(".img3").setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
-                document.querySelector(".img4").setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
-                document.querySelector(".img5").setAttribute("style", "animation: hithere 1s ease; opacity: 0.5;");
-
                 // set random number and display dice
-                document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png"); 
-                document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png"); 
-                document.querySelector(".img3").setAttribute("src", "images/dice" + randomNumber3 + ".png"); 
-                document.querySelector(".img4").setAttribute("src", "images/dice" + randomNumber4 + ".png"); 
-                document.querySelector(".img5").setAttribute("src", "images/dice" + randomNumber5 + ".png"); 
+                d1.setAttribute("src", "images/dice" + randomNumber1 + ".png"); 
+                d2.setAttribute("src", "images/dice" + randomNumber2 + ".png"); 
+                d3.setAttribute("src", "images/dice" + randomNumber3 + ".png"); 
+                d4.setAttribute("src", "images/dice" + randomNumber4 + ".png"); 
+                d5.setAttribute("src", "images/dice" + randomNumber5 + ".png"); 
 
                 setTimeout(function() {
                         //clear animation
-                        document.querySelector(".img1").setAttribute("style", "none;");
-                        document.querySelector(".img2").setAttribute("style", "none;");
-                        document.querySelector(".img3").setAttribute("style", "none;");
-                        document.querySelector(".img4").setAttribute("style", "none;");
-                        document.querySelector(".img5").setAttribute("style", "none;");
+                        d1.setAttribute("style", "none;");
+                        d2.setAttribute("style", "none;");
+                        d3.setAttribute("style", "none;");
+                        d4.setAttribute("style", "none;");
+                        d5.setAttribute("style", "none;");
 
 
                 }, 1000)
 
                 // set score 
-                let diceSum = (d1Value + d2Value + d3Value + d4Value + d5Value);
-                let rollScore = document.querySelector(".rollScore");
-                rollScore.innerHTML = diceSum; 
+                calScore();
 
+                };
         }, 1000); 
-
+        
 } 
 
